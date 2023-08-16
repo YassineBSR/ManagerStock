c54193ef6054a4399ad9b37fdf5e93e7c869c42c
Yassine B <yassine60160@gmail.com>
2023-08-16 15:54:29 +0200

    stable

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -18,7 +18,7 @@ class SearchType extends AbstractType
                     'placeholder' => 'Recherche via un mot clé ...',
                     'type' => 'search',
                     'id' => 'default-search',
-                    'class' => 'block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    'class' => 'block w-full p-4 pl-10 text-sm text-white border-transparent placeholder:white rounded-lg bg-teal-700 focus:ring-teal-500 focus:border-teal-500'
                 ]
                 ]);
     }
